ab22ece3dcfbb141619115976d8d655b5cf1b370
next-davidespinola <david.espinola.next@bbva.com>
2020-11-26 13:01:21 +0100

    Inventory grid mocked

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,14 +1,55 @@
 <template>
-  <div>Inventory</div>
+  <v-container>
+    <v-row>
+      <v-col v-for="(item, index) in items" :key="index" cols="6">
+        <ShopItem v-bind="item" :button-disabled="item.used" :button-text="buttonText(item)" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import ShopItem from '../components/ShopItem.vue'
+
 export default {
   name: 'Inventory',
-  components: {},
+  components: { ShopItem },
   data() {
-    return {}
+    return {
+      items: [
+        {
+          id: 3,
+          type: 'pet',
+          name: 'Gato',
+          requiredLevel: 2,
+          used: true
+        },
+        {
+          id: 5,
+          type: 'background',
+          name: 'Fondo cielo',
+          requiredLevel: 2,
+          used: true
+        },
+        {
+          id: 1,
+          type: 'pet',
+          name: 'Abeja',
+          requiredLevel: 3
+        },
+        {
+          id: 2,
+          type: 'background',
+          name: 'Fondo espacial',
+          requiredLevel: 3
+        }
+      ]
+    }
   },
-  methods: {}
+  methods: {
+    buttonText(item) {
+      return item.used ? 'Actual' : 'Usar'
+    }
+  }
 }
 </script>
