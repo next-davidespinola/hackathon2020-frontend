2039b7b06c6d170f47b43d9b917564e167d7bcd1
Karina De Sousa <karina.desousa.next@bbva.com>
2020-11-26 13:45:23 +0100

    feat: custom component for upper section integration on shop page

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,24 +1,48 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col v-for="(item, index) in items" :key="index" cols="6">
-        <ShopItem v-bind="item" :disabled="!hasRequiredLevel(item)" button-text="Comprar" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <ImageInfoSection
+      class="d-flex flex-column text-center"
+      title="Mis gemas"
+      titleIcon="mdi-diamond-stone"
+      titleColor="purple"
+      :imgSrc="imgSrc"
+      :cardColor="'deep-purple darken-6'"
+    >
+      <div class="pb-4 pr-4 pl-4">
+        <p class="text-h4 white--text font-weight-medium"> 800 Gemas </p>
+        <p class="mt-5 font-weight-regular text-h6 grey--text text--lighten-3"> ¡Tus ahorros generan interés! Ganas <b> 8 Gemas </b> cada viernes </p>
+      </div>
+    </ImageInfoSection>
+
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, index) in items" :key="index" cols="6">
+          <ShopItem v-bind="item" :disabled="!hasRequiredLevel(item)" button-text="Comprar" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import ImageInfoSection from '@/components/ImageInfoSection.vue'
 import ShopItem from '../components/ShopItem.vue'
 
 export default {
   name: 'Shop',
-  components: { ShopItem },
+  components: {
+    ImageInfoSection,
+    ShopItem
+  },
   props: {
-    data: { type: Object, required: true }
+    data: {
+      type: Object,
+      required: true
+    }
   },
   data() {
     return {
+      imgSrc: 'gems.png',
       items: [
         {
           id: 3,
