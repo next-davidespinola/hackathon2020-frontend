eecee7285c3b17381914eb4b9170ee6c7e9573b9
Karina De Sousa <karina.desousa.next@bbva.com>
2020-11-26 14:00:11 +0100

    feat: custom component for upper section integration on invetory page

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -1,21 +1,43 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col v-for="(item, index) in items" :key="index" cols="6">
-        <ShopItem v-bind="item" :button-disabled="item.used" :button-text="buttonText(item)" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <ImageInfoSection
+      class="d-flex flex-column text-center"
+      title="Mi nivel"
+      titleIcon="mdi-bag-personal"
+      titleColor="blue"
+      :imgSrc="imgSrc"
+      :cardColor="'deep-blue'"
+    >
+      <div class="pt-2 pb-2 pr-4 pl-4">
+        <p class="mb-1 font-weight-regular text-h6 grey--text text--lighten-3"> ¡Tines muchas cosas en tu mochila! </p>
+        <p class="mb-1 text-h4 white--text font-weight-medium"> 2 fondos </p>
+        <p class="text-h4 white--text font-weight-medium"> 2 mascotas </p>
+      </div>
+    </ImageInfoSection>
+
+    <v-container>
+      <v-row>
+        <v-col v-for="(item, index) in items" :key="index" cols="6">
+          <ShopItem v-bind="item" :button-disabled="item.used" :button-text="buttonText(item)" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import ImageInfoSection from '@/components/ImageInfoSection.vue'
 import ShopItem from '../components/ShopItem.vue'
 
 export default {
   name: 'Inventory',
-  components: { ShopItem },
+  components: {
+    ImageInfoSection,
+    ShopItem
+  },
   data() {
     return {
+      imgSrc: 'bag.png',
       items: [
         {
           id: 3,
